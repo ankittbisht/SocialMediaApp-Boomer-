4667ded42e6c8d5ad07b55253d2c281496a78a08
ankit bisht <ab67743@gmail.com>
2023-07-09 19:55:12 +0530

    client env creation

--- client/src/components/Friend.jsx
+++ client/src/components/Friend.jsx
@@ -24,7 +24,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 //api call to add or remove friend.
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:3001/users/${_id}/${friendId}`,
+      `${process.env.REACT_APP_SERVER_URL}/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
